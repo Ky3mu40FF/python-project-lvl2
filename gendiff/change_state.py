86c65b8fa7ca5313d1c3e644bccee5cc2ace8116
Алексей Фоминых <foma1994@yandex.ru>
2022-05-10 14:14:37 +0300

    Step 6 (ref) - changed diff generator

---
+++ gendiff/change_state.py
@@ -0,0 +1,7 @@
+"""change_state module."""
+UNCHANGED = 'unchanged'
+CHANGED_BEFORE = 'changed_before'
+CHANGED_AFTER = 'changed_after'
+ADDED = 'added'
+REMOVED = 'removed'
+NESTED = 'nested'
--- gendiff/generator.py
+++ gendiff/generator.py
@@ -1,76 +1,111 @@
 """generator module."""
 
+from .change_state import (
+    ADDED,
+    CHANGED_AFTER,
+    CHANGED_BEFORE,
+    NESTED,
+    REMOVED,
+    UNCHANGED,
+)
 from .formatters import FORMATTERS
 from .parser import get_parsed_file
 
 
-def compare_data(data1, data2):
-    """Compare two data.
-    
+def set_state(state, key, child):
+    return {(state, key): child}
+
+
+def compare_children(key, child1, child2):
+    children_comparison_result = {}
+    any_nested = isinstance(child1, dict) or isinstance(child2, dict)
+
+    if child1 == child2:
+        children_comparison_result.update(set_state(
+            UNCHANGED,
+            key,
+            child1,
+        ))
+    # If none of passed childs is nested and childs are not equal
+    elif not (any_nested) and (child1 != child2):
+        children_comparison_result.update(set_state(
+            CHANGED_BEFORE,
+            key,
+            child1,
+        ))
+        children_comparison_result.update(set_state(
+            CHANGED_AFTER,
+            key,
+            child2,
+        ))
+    elif any_nested and child1 != child2:
+        children_comparison_result.update(set_state(
+            NESTED,
+            key,
+            get_change_states(child1, child2),
+        ))
+    return children_comparison_result
+
+
+def get_change_states(data1, data2):
+    """
+    Get change states between two datasets.
+
     Args:
-        data1 (): DATA1.
-        data2 (): DATA2.
+        data1 (dict): DATA1.
+        data2 (dict): DATA2.
 
     Returns:
-        (dict): COMPARE RESULT."""
-    result = {}
-    common_keys = data1.keys() & data2.keys()
-    removed_keys = data1.keys() - data2.keys()
-    added_keys = data2.keys() - data1.keys()
+        (dict): COMPARE RESULT.
+    """
+    data_with_change_states = {}
 
-    for key in common_keys:
-        if isinstance(data1[key], dict) and isinstance(data2[key], dict):
-            result[key] = {
-                'status': 'NESTED',
-                'value': compare_data(data1[key], data2[key]),
-            }
-        elif data1[key] == data2[key]:
-            result[key] = {
-                'status': 'EQUAL',
-                'value': data1[key],
-            }
-        else:
-            result[key] = {
-                'status': 'MODIFIED',
-                'old_value': compare_data(data1[key], data1[key]) if isinstance(data1[key], dict) else data1[key],
-                'value': compare_data(data2[key], data2[key]) if isinstance(data2[key], dict) else data2[key],
-            }
+    for common_key in data1.keys() & data2.keys():
+        data_with_change_states.update(compare_children(
+            common_key,
+            data1[common_key],
+            data2[common_key],
+        ))
 
-    for key in removed_keys:
-        value = compare_data(data1[key], data1[key]) if isinstance(data1[key], dict) else data1[key]
-        result[key] = {
-                'status': 'REMOVED', 
-                'value': value,
-            }
+    for removed_key in data1.keys() - data2.keys():
+        data_with_change_states.update(set_state(
+            REMOVED,
+            removed_key,
+            data1[removed_key],
+        ))
 
-    for key in added_keys:
-        value = compare_data(data2[key], data2[key]) if isinstance(data2[key], dict) else data2[key]
-        result[key] = {
-                'status': 'ADDED', 
-                'value': value,
-            }
+    for added_key in data2.keys() - data1.keys():
+        data_with_change_states.update(set_state(
+            ADDED,
+            added_key,
+            data2[added_key],
+        ))
 
-    return result
+    return data_with_change_states
 
 
 def format_diff(diff, formatter):
-    """Format diff with selected formatter.
-    
+    """
+    Format diff with selected formatter.
+
     Args:
         diff (dict): Dict with diff between two datas.
         formatter (str): String with formatter name.
-    
+
     Returns:
-        (str): Formatted diff."""
+        (str): Formatted diff.
+    """
     return formatter(diff)
 
 
 def generate_diff(first_file, second_file, formatter):
-    """Generate string with diffs between two files.
+    """
+    Generate string with diffs between two files.
 
     Args:
         first_file (dict): Dict of first parsed file.
         second_file (dict): Dict of second parsed file.
+        formatter (string): SELECTED FORMATTER NAME.
 
     Returns:
         (str): String with differences of two files.
@@ -87,6 +122,6 @@ def generate_diff(first_file, second_file, formatter):
         print(file2_exception_info)
         return ''
 
-    diff = compare_data(data1, data2)
+    diff = get_change_states(data1, data2)
 
     return format_diff(diff, FORMATTERS[formatter])
