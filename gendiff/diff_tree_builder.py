e0f1ca67b4e0562e4a1fdfcba397966e914b46e0
Алексей Фоминых <foma1994@yandex.ru>
2022-08-09 01:08:55 +0300

    3rd CR - Fixed 17 remark

--- gendiff/diff_tree_builder.py
+++ gendiff/diff_tree_builder.py
@@ -1,7 +1,4 @@
 """diff_tree_generator module."""
-# SimpleNamespace is None alternative,
-# because None is representing null after json parsing
-from types import SimpleNamespace
 
 from gendiff.change_type import (
     ADDED,
@@ -28,53 +25,36 @@ def compare_tree(tree1, tree2):
         (dict): Compare result - Dictionary with differences.
     """
     differences = {}
-    # Child not found in tree1
-    # -> Property was added.
-    if type(tree1) is SimpleNamespace:
-        return {
-            DIFF_CHANGE_TYPE: ADDED,
-            DIFF_VALUE: tree2,
-        }
-    # Child not found in tree2
-    # -> Property was removed.
-    elif type(tree2) is SimpleNamespace:
-        return {
-            DIFF_CHANGE_TYPE: REMOVED,
-            DIFF_VALUE: tree1,
-        }
-    # One of the children or both of them are not dicts and they are not equal
-    # -> Property was changed.
-    elif type(tree1) != type(tree2) or not isinstance(tree1, dict) and tree1 != tree2:
-        return {
-            DIFF_CHANGE_TYPE: CHANGED,
-            DIFF_VALUE: {
-                CHANGED_FROM: tree1,
-                CHANGED_TO: tree2,
-            },
-        }
-    # Both children are equal
-    # -> Property unchanged.
-    elif tree1 == tree2:
-        return {
-            DIFF_CHANGE_TYPE: UNCHANGED,
-            DIFF_VALUE: tree1,
-        }
-    # Both children are dicts and not equal
-    # -> Nested properties need to be compared deeper.
-    else:
-        for key in sorted(set().union(tree1.keys(), tree2.keys())):
-            # Try to get child from trees at given key.
-            # If key is not exists in specific tree - return SimpleNamespace instance.
-            # It's alternative to None to avoid collision with converted null from JSON.
-            tree1_value = tree1.get(key, SimpleNamespace())
-            tree2_value = tree2.get(key, SimpleNamespace())
-            nested_compare_result = compare_tree(tree1_value, tree2_value)
-            if isinstance(tree1_value, dict) and isinstance(tree2_value, dict):
-                differences[key] = {
-                    DIFF_CHANGE_TYPE: NESTED,
-                    DIFF_VALUE: nested_compare_result,
-                }
-            else:
-                differences[key] = nested_compare_result
+
+    for key in sorted(set().union(tree1.keys(), tree2.keys())):
+
+        if key not in tree1.keys() and key in tree2.keys():
+            differences[key] = {
+                DIFF_CHANGE_TYPE: ADDED,
+                DIFF_VALUE: tree2[key],
+            }
+        elif key in tree1.keys() and key not in tree2.keys():
+            differences[key] = {
+                DIFF_CHANGE_TYPE: REMOVED,
+                DIFF_VALUE: tree1[key],
+            }
+        elif tree1[key] == tree2[key]:
+            differences[key] = {
+                DIFF_CHANGE_TYPE: UNCHANGED,
+                DIFF_VALUE: tree1[key],
+            }
+        elif isinstance(tree1[key], dict) and isinstance(tree2[key], dict):
+            differences[key] = {
+                DIFF_CHANGE_TYPE: NESTED,
+                DIFF_VALUE: compare_tree(tree1[key], tree2[key]),
+            }
+        else:
+            differences[key] = {
+                DIFF_CHANGE_TYPE: CHANGED,
+                DIFF_VALUE: {
+                    CHANGED_FROM: tree1[key],
+                    CHANGED_TO: tree2[key],
+                },
+            }
 
     return differences
